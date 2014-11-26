4fbe7dfc0c2202284e237a94eceb0c99e7b0b988
Maciej Stopczyk <maciek.stopczyk@gmail.com>
2014-11-26 09:20:03 +0100

    s

--- makerelease.sh
+++ makerelease.sh
@@ -7,6 +7,6 @@ git flow release start 1.0.9
 #echo "$last" >version
 #git commit -am "update version"
 git flow release publish 1.0.9
-git flow release finish -mversion 1.0.9
+git flow release finish -m "version" 1.0.9
 git push && git push --tags
 
