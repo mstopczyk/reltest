ec10955fbf8c18187515c2e8896629cc06204402
Maciej Stopczyk <maciek.stopczyk@gmail.com>
2014-11-26 09:20:27 +0100

    s

--- makerelease.sh
+++ makerelease.sh
@@ -3,10 +3,10 @@
 #last=`cat version`
 #last=$((last + 1))
 
-git flow release start 1.0.9
+git flow release start 1.0.10
 #echo "$last" >version
 #git commit -am "update version"
-git flow release publish 1.0.9
-git flow release finish -m "version" 1.0.9
+git flow release publish 1.0.10
+git flow release finish -m "version" 1.0.10
 git push && git push --tags
 
